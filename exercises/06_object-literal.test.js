db79d66f10f7306ffca23986ecc8a05f2d30fd26
Mykola Kodrul <akella.n@gmail.com>
2017-09-03 22:47:30 +0300

    pass exercise 07_parameters

--- exercises/06_object-literal.test.js
+++ exercises/06_object-literal.test.js
@@ -3,9 +3,9 @@ test('can use shorthand for property names', () => {
     //Using NEW Object Literal Syntax, return a literal that will allow the tests to pass
     return {
       type: 'Monster',
-      name: name,
-      power: power,
-      attack: function (target){
+      name,
+      power,
+      attack(target){
         return `${this.name} attacked ${target.name}`;
       }
     }
