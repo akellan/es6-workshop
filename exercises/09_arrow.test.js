2b32553c8cecc7d3bdab4a5f5f64154012bd1d75
Mykola Kodrul <akella.n@gmail.com>
2017-09-04 22:24:04 +0300

    pass exercise 09_arrow

--- exercises/09_arrow.test.js
+++ exercises/09_arrow.test.js
@@ -1,6 +1,11 @@
 test('can replace traditional functions', () => {
   let fnMultiply, arrowMultiply
 
+  fnMultiply = function (a, b) {
+    return a * b
+  }
+
+  arrowMultiply = (a, b) => a * b
   // Write two functions that take two params and return their product
   // For 'fnMultiply', set it equal to a regular function
   // For 'arrowMultiply', set it equal to an arrow function
@@ -12,9 +17,7 @@ test('can replace traditional functions #2', () => {
   const nums = [2, 5, 10]
   // Replace the 'function' in this 'map' call with an arrow function.
   // Hint: you shouldn't have any braces or 'return' after you are done
-  const squares = nums.map(function(num) {
-    return num * num
-  })
+  const squares = nums.map(num => num * num)
 
   expect(squares.shift()).toBe(4)
   expect(squares.shift()).toBe(25)
@@ -26,10 +29,10 @@ test('binds `this` to the eval scope, not the runtime scope', () => {
   // allow for 'this' to retain context correctly
   const person = {
     name: 'Aaron',
-    greetFriends: function(friends) {
-      return friends.map(function(friend) {
-        return this.name + ' greets to ' + friend
-      })
+    greetFriends: function (friends) {
+      return friends.map(friend =>
+        this.name + ' greets to ' + friend
+      )
     },
   }
 
@@ -39,31 +42,23 @@ test('binds `this` to the eval scope, not the runtime scope', () => {
 
 test('can make array filter chains more managable', () => {
   const data = [
-    {type: 'Widget', name: 'Sprocket', price: 10.00, qty: 3},
-    {type: 'Widget', name: 'Bracket', price: 1.00, qty: 5},
-    {type: 'Widget', name: 'Brace', price: 2.50, qty: 1},
-    {type: 'Widget', name: 'Sprocket', price: 4.00, qty: 2},
-    {type: 'Food', name: 'Gouda', price: 8.75, qty: 4},
-    {type: 'Food', name: 'Bacon', price: 3.50, qty: 3},
-    {type: 'CD', name: 'Queen Best Hits', price: 5.50, qty: 5},
-    {type: 'CD', name: 'Brittney Best Hits', price: 6.25, qty: 3},
-    {type: 'CD', name: 'JT Best Hits', price: 2.25, qty: 6},
+    { type: 'Widget', name: 'Sprocket', price: 10.00, qty: 3 },
+    { type: 'Widget', name: 'Bracket', price: 1.00, qty: 5 },
+    { type: 'Widget', name: 'Brace', price: 2.50, qty: 1 },
+    { type: 'Widget', name: 'Sprocket', price: 4.00, qty: 2 },
+    { type: 'Food', name: 'Gouda', price: 8.75, qty: 4 },
+    { type: 'Food', name: 'Bacon', price: 3.50, qty: 3 },
+    { type: 'CD', name: 'Queen Best Hits', price: 5.50, qty: 5 },
+    { type: 'CD', name: 'Brittney Best Hits', price: 6.25, qty: 3 },
+    { type: 'CD', name: 'JT Best Hits', price: 2.25, qty: 6 },
   ]
 
   // REPLACE ALL REGULAR FUNCTIONS WITH ARROW FUNCTIONS
   const shoppingList = data
-    .filter(function(d) {
-      return d.type != 'Widget'
-    }) // Remove Widgets
-    .filter(function(d) {
-      return d.price < 5
-    }) // Find only remaining items with price < 5
-    .sort(function(d) {
-      return d.qty * -1
-    }) // Sort by price, desc
-    .map(function(d) {
-      return d.name
-    }) // Pull just the name from each item
+    .filter(d => d.type != 'Widget') // Remove Widgets
+    .filter(d => d.price < 5) // Find only remaining items with price < 5
+    .sort(d => d.qty * -1) // Sort by price, desc
+    .map(d => d.name) // Pull just the name from each item
 
   expect(shoppingList.shift()).toBe('Bacon')
   expect(shoppingList.shift()).toBe('JT Best Hits')
@@ -74,7 +69,7 @@ test('can make array filter chains more managable', () => {
 http://ws.kcd.im/?ws=ES6+and+Beyond&e=Arrow+Functions&em=
 */
 test('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+  const submitted = true // change this when you've submitted!
   expect(true).toBe(submitted)
 })
 ////////////////////////////////
